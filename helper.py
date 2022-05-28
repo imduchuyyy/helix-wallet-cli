51ffa8dd6f037d35da3d7e9a837ce40d6cf520ba
Bui Duc Huy <duchuy.124dk@gmail.com>
2022-05-29 01:00:17 +0700

    feat(wallet): transfer eth, transfer token

--- helper.py
+++ helper.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
-import sys, time
+import sys, time, os
+from os import walk
 from eth_account import Account
 import json
+from web3 import Web3
+import web3
+from constants import *
+
+provider = Web3.HTTPProvider('https://mainnet.infura.io/v3/9e4bc49c44c34ac7ae3e5c34fe5e1d62')
+w3 = Web3(provider)
 
 def type_writer(message, delay_time):
     for char in message:
@@ -10,14 +17,82 @@ def type_writer(message, delay_time):
         sys.stdout.flush()
         time.sleep(delay_time)
 
+    time.sleep(0.1)
+
 def create_new_key(private_key, password):
     account = Account.from_key(private_key)
     encrypted_key = Account.encrypt(private_key, password = password)
 
     json_object = json.dumps(encrypted_key, indent = 4)
 
-    with open(account.address + ".keypair" + ".json", "w") as outfile:
+    with open(".keypair" + ".json", "w") as outfile:
         outfile.write(json_object)
+
+def decrypt_wallet(wallet, password):
+    decrypt_key = Account.decrypt(wallet, password)
+
+    return decrypt_key
+
+def get_wallet_encrypt():
+    with open(".keypair.json", 'r') as openfile:
+        wallet = json.load(openfile)
+    return wallet
 
 def get_wallet_address_from_file_name(name):
     return name.split(".")[0]
+
+def get_wallet_by_index(index):
+    wallets = get_wallets_list()
+    return wallets[index - 1]
+
+def to_checksum_address(address):
+    return Web3.toChecksumAddress(address)
+
+
+def get_token_info(wallet_address, token_address):
+    token_info = {}
+
+    if len(token_address) == 0:
+        token_info["address"] = "0x0000000000000000000000000000000000000000"
+        token_info["name"] = "Ethereum"
+        token_info["symbol"] = "ETH"
+        token_info["decimals"] = "18"
+        token_info["balance"] = str(float(w3.eth.get_balance(wallet_address)) / 10 ** 18)
+
+    else:
+        token = w3.eth.contract(address=to_checksum_address(token_address), abi=ERC20_ABI)
+
+        token_info["address"] = token_address
+        token_info["name"] = str(token.functions.name().call())
+        token_info["symbol"] = str(token.functions.symbol().call())
+        token_info["decimals"] = str(token.functions.decimals().call())
+        token_info["balance"] = str(token.functions.balanceOf(wallet_address).call() / 10 ** int(token_info["decimals"]))
+
+    return token_info
+
+def transfer_token(token_info, address, private_key, receiver_address, amount):
+    token = w3.eth.contract(address=to_checksum_address(token_info["address"]), abi=ERC20_ABI)
+    amount = int(amount * (10 ** int(token_info["decimals"])))
+    nonce = w3.eth.getTransactionCount(address)
+
+    transaction = token.functions.transfer(receiver_address, amount).buildTransaction({'nonce': nonce, 'gas': 70000, 'gasPrice': w3.toWei('10', 'gwei'),})
+
+    signed_txn = w3.eth.account.signTransaction(transaction, private_key=private_key)
+    w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+
+    return signed_txn.hash
+
+def transfer_eth(address, private_key, receiver_address, amount):
+    nonce = w3.eth.getTransactionCount(address)
+    tx = {
+        'nonce': nonce,
+        'to': receiver_address,
+        'value': w3.toWei(amount, 'ether'),
+        'gas': 2000000,
+        'gasPrice': w3.toWei('10', 'gwei')
+    }
+
+    signed_tx = w3.eth.account.sign_transaction(tx, private_key)
+    tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+
+    return tx_hash
--- wallet.py
+++ wallet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-import web3
-import os
+from web3 import Web3
+import os, time
 from os import walk
 import sys
 import secrets
@@ -11,27 +11,22 @@ from constants import *
 from helper import *
 
 def print_hello_text():
-    type_writer(BOLD + GRN + "\n   [ Mini Cool Wallet ] - by @terry.h \n" + CEND + "\n", 0.01)
+    print(BOLD + GRN + "\n   [ Mini Cool Wallet ] - by @terry.h \n" + CEND + "\n")
 
-def get_wallets_handler():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    filenames = next(walk(dir_path), (None, None, []))[2]
-
-    keypairs = [filename for filename in filenames if ".keypair" in filename ]
-
-    if len(keypairs) == 0:
+def get_wallet_handler():
+    try:
+        wallet = get_wallet_encrypt()
+        checksum_address = to_checksum_address("0x" + wallet["address"])
+        print(BOLD + CBLINK + "Wallet Found: ", end='')
+        type_writer(BOLD + CYELLOW + checksum_address  + CEND + "\n", 0.01)
+    except:
         type_writer(BOLD + CRED + "\n ❌ No Wallet" + CEND + "\n", 0.01)
         return
 
-    type_writer(BOLD + CYELLOW + "\n > List Wallet:" + CEND + "\n", 0.01)
-
-    for i, wallet_file in enumerate(keypairs):
-        print(BOLD + CBLINK + "\n > Wallet " + str(i + 1) + ":" + CEND, end='')
-        type_writer(BOLD + CYELLOW + get_wallet_address_from_file_name(wallet_file) + CEND, 0.01)
 
 def new_wallet_handler():
     type_writer(BOLD + CYELLOW + "\n > Create New Wallet" + CEND , 0.01)
-    priv = secrets.token_hex(32)
+    priv = secrets.token_hex(32) 
     private_key = "0x" + priv
     account = Account.from_key(private_key)
     print(BOLD + CBLINK + "\n > New Wallet: ", end='')
@@ -53,9 +48,71 @@ def new_wallet_handler():
 
     type_writer(BOLD + GRN + "\n   New Wallet Created !!! \n" + CEND + "\n", 0.01)
 
+def print_token_info(token_info):
+    type_writer(BOLD + CYELLOW + "\n > Token Found:" + CEND + "\n", 0.01)
+    print("       > Token Address: ", end="")
+    type_writer(BOLD + CYELLOW + token_info["address"] + CEND + "\n", 0.01)
+    print("       > Token Name: ", end="")
+    type_writer(BOLD + CYELLOW + token_info["name"] + CEND + "\n", 0.01)
+    print("       > Token Symbol: ", end="")
+    type_writer(BOLD + CYELLOW + token_info["symbol"] + CEND + "\n", 0.01)
+    print("       > Token Decimals: ", end="")
+    type_writer(BOLD + CYELLOW + token_info["decimals"] + CEND + "\n", 0.01)
+    print("       > Balance: ", end="")
+    if float(token_info["balance"]) > 0: 
+        type_writer(BOLD + CYELLOW + token_info["balance"] + CEND + "\n", 0.01)
+    else: 
+        type_writer(BOLD + CRED + token_info["balance"] + CEND + "\n", 0.01)
+
+def unlock_wallet(wallet):
+    is_correct_password = False
+
+    while not is_correct_password: 
+        type_writer(BOLD + CYELLOW + "\n > Enter password: " + CEND , 0.01)
+        password = getpass("")
+        try:
+            key = decrypt_wallet(wallet, password)
+            is_correct_password = True
+        except:
+            type_writer(BOLD + CRED + "❌ Wrong password" + CEND, 0.01)
+
+    return key
+
 
 def transfer_handler():
-    type_writer(BOLD + CYELLOW + "\n > Transfer" + CEND + "\n", 0.01)
+    type_writer(BOLD + CYELLOW + "> Transfer Token" + CEND + "\n", 0.01)
+
+    type_writer(BOLD + CYELLOW + "\n > Token Address (Default is Native token): " + CEND , 0.01)
+    token_address = input()
+
+    wallet = get_wallet_encrypt()
+
+    type_writer(BOLD + CCYAN + "\n > Fetching Token Info ... " + CEND , 0.01)
+    checksum_address = to_checksum_address("0x" + wallet["address"])
+    token_info = get_token_info(checksum_address, token_address)
+
+    print_token_info(token_info)
+
+    type_writer(BOLD + CYELLOW + "\n > Receiver Address: " + CEND , 0.01)
+    receiver_address = input()
+
+    type_writer(BOLD + CYELLOW + "> Amount: " + CEND , 0.01)
+    amount = input()
+
+    key = unlock_wallet(wallet)
+
+
+    try:
+        if len(token_address) == 0:
+            hash = transfer_eth(checksum_address, key, receiver_address, float(amount))
+        else:
+            hash = transfer_token(token_info, checksum_address, key, receiver_address, float(amount))
+        print(BOLD + GRN + "\n       Transaction Success: ", end="")
+        type_writer(BOLD + GRN + str(hash.hex()) + CEND + "\n", 0.01)
+    except e:
+        print(e)
+        type_writer(BOLD + CRED + "\n       ❌ Transaction Fail" + CEND, 0.01)
+
 
 def main():
     print_hello_text()
@@ -63,8 +120,8 @@ def main():
     arguments = docopt(doc, argv=None, help=True, options_first=False)
     if arguments["new"]: 
         new_wallet_handler()
-    if arguments["list"]:
-        get_wallets_handler()
+    if arguments["address"]:
+        get_wallet_handler()
     if arguments["transfer"]:
         transfer_handler()
 
