90509a4e9f12e93c214dfeeb9b8cca27e6de563b
Doan Bui <bxdoan93@gmail.com>
2022-12-03 00:54:48 +0700

    add test symbols and decimals

--- tests/test_token.py
+++ tests/test_token.py
@@ -4,22 +4,18 @@ import pytest
 from tartarus.constants import ETH_NATIVE_ADDRESS
 from tartarus.token import Token
 
+TOKEN_NAME = "BuiCoin"
+TOKEN_SYMBOL = "BUI"
+TOKEN_DECIMALS = 18
+TOKEN_INITIAL_SUPPLY = 0
+
 
 @pytest.fixture
 def token_contract(w3, get_contract):
     code = """
-symbol: public(String[32])
-decimals: public(uint256)
-
 @external
 @view
 def get_balance() -> uint256:
-    a: uint256 = self.balance
-    return a
-
-@external
-@view
-def symbol() -> uint256:
     a: uint256 = self.balance
     return a
 
@@ -30,6 +26,24 @@ def __default__():
     """
     contract = get_contract(code, *[w3.eth.accounts[0]])
     return contract
+
+
+@pytest.fixture
+def token_cont(w3, get_contract):
+    code = """
+name: public(String[32])
+symbol: public(String[32])
+decimals: public(uint8)
+
+@external
+def __init__(_name: String[32], _symbol: String[32], _decimals: uint8):
+    self.name = _name
+    self.symbol = _symbol
+    self.decimals = _decimals
+
+    """
+    c = get_contract(code, *[TOKEN_NAME, TOKEN_SYMBOL, TOKEN_DECIMALS])
+    return c
 
 
 def test_get_balance_ETH(w3, token_contract):
@@ -47,6 +61,32 @@ def test_get_balance_ETH(w3, token_contract):
     )
     balance = t.get_balance()
     assert balance == f'{value / 10 ** 18}'
+
+
+def test_get_symbol_and_decimals(w3, token_cont):
+    url = 'https://some_url.com'  # mainnet
+    # Check total supply, name, symbol and decimals are correctly set
+    assert token_cont.name() == TOKEN_NAME
+    assert token_cont.symbol() == TOKEN_SYMBOL
+    assert token_cont.decimals() == TOKEN_DECIMALS
+
+    # check symbol
+    t1 = Token(
+        w3=w3,
+        url=url,
+        wallet_address=token_cont.address,
+        token_address=token_cont.address,
+    )
+    assert t1.get_symbol() == TOKEN_SYMBOL
+
+    # check decimals
+    t2 = Token(
+        w3=w3,
+        url=url,
+        wallet_address=token_cont.address,
+        token_address=token_cont.address,
+    )
+    assert t2.get_decimal() == TOKEN_DECIMALS
 
 
 @pytest.mark.skip(reason='Call external service')
